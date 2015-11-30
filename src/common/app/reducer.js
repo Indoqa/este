b2833ffbe658951fdfb7839fe2b55b14f16cfbf1
mepheser <matthias.epheser@indoqa.com>
2015-11-30 18:58:18 +0100

    remove reducers "ui" and "display" as they are only used by native components

--- src/common/app/reducer.js
+++ src/common/app/reducer.js
@@ -3,25 +3,12 @@ import {combineReducers} from 'redux'
 // Note we are composing all reducers. Web, native, whatever. Of course we can
 // pass platform specific reducers in configureStore, but there is no reason to
 // do that, until app is really large.
-<<<<<<< HEAD
 import intl from '../intl/reducer'
 import home from '../home/reducer'
-=======
-import device from '../device/reducer';
-import intl from '../intl/reducer';
-import home from '../home/reducer';
-import ui from '../ui/reducer';
->>>>>>> * Removed unnecessary resources
 
 const appReducer = combineReducers({
   intl,
-<<<<<<< HEAD
   home
-})
-=======
-  home,
-  ui
 });
->>>>>>> * Removed unnecessary resources
 
 export default appReducer
