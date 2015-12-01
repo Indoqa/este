e62e5e2322eaa93117663703b7b43d42d0bfc5f1
mepheser <matthias.epheser@indoqa.com>
2015-12-01 11:31:37 +0100

    remove @fetch

--- src/browser/home/Page.react.js
+++ src/browser/home/Page.react.js
@@ -4,7 +4,6 @@ import Component from 'react-pure-render/component'
 import Helmet from 'react-helmet'
 import React, {PropTypes} from 'react'
 
-@fetch(updateTime)
 export default class Page extends Component {
 
   static propTypes = {
